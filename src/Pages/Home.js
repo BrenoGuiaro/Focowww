ca622196182dbcb502732ce5e4eb692a5ee24f27
Fiote <brenoft14@gmail.com>
2024-02-11 17:43:39 -0300

    vendo como esta no celular

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,10 +4,20 @@ import '../CSS/Home.css'
 export const Home = () => {
   return (
     <>
-      <div className='alim'>
-        <p className='textA'>ALIMENTAÇÃO</p>
+      <div className='divBtn'>
+        <ul className='listBtn'>
+          <button className='btn'>A</button>
+          <button className='btn'>T</button>
+          <button className='btn'>M</button>
+          <button className='btn'>P</button>
+        </ul>
+        <ul className='divP'>
+          <p className='p1'>Alimentação</p>
+          <p className='p'>Treino</p>
+          <p className='p2'>Metas</p>
+          <p className='p'>Progresso</p>
+        </ul>
       </div>
-      <p className='R'>ROBERTO</p>
     </>
 
   )

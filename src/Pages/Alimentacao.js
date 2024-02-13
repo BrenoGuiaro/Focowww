e1b9233144a375a86b2505c556465d12c3b66c65
Fiote <brenoft14@gmail.com>
2024-02-13 20:44:27 -0300

    botao instalar 1.0

--- src/Pages/Alimentacao.js
+++ src/Pages/Alimentacao.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import '../CSS/Alimentacao.css'
+import { IoMdDownload  as Download} from "react-icons/io";
+import { FaGooglePlay as Play } from "react-icons/fa";
+import { FaApple as Apple } from "react-icons/fa";
 
 export const Alimentacao = () => {
+
   return (
     <div className='fundoGro'>
       <div className='boxTitle'>
-        Alimentação
+       <p className='ti'>Alimentação</p> 
+      </div>
+
+      <div className='aplicativo'>
+        <div className='imgAp'>
+          <div className='baixo'>
+            <p className='name'>Aplicativo</p>
+            <p className='inst'><Download className='dow'/>Instalar</p>
+            <div className='info'>
+              <div className='divPlay'>
+                <Play className='play'/>
+                <p className='textPlay'>Instalar</p>
+              </div>
+              
+                <div className='divApple'>
+                  <Apple className='apple'/>
+                  <p className='textPlay'>Instalar</p>
+                </div>
+
+                <p className='saiba'>Saiba mais...</p>
+              
+            </div>
+          </div>
+        </div>
       </div>
 
 
